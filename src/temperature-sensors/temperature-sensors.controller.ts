466357260fda89a4cd4a35c604d0783c71d7c544
Olesia Bosa <olesia.olecsandrova@gmail.com>
2026-09-16 18:16:36 +0300

    fix2 sse: sse before id in controller

--- src/temperature-sensors/temperature-sensors.controller.ts
+++ src/temperature-sensors/temperature-sensors.controller.ts
@@ -22,6 +22,15 @@ export class TemperatureSensorsController {
     return this.temperatureSensorsService.findAll();
   }
 
+    @Sse('alerts')
+  getAlerts(): Observable<MessageEvent> {
+    return this.alertsService.getAlertStream().pipe(
+      map((alert) => ({
+        data: alert,
+      })),
+    );
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.temperatureSensorsService.findOne(id);
@@ -40,12 +49,5 @@ export class TemperatureSensorsController {
     return this.temperatureSensorsService.remove(id);
   }
 
-  @Sse('alerts')
-  getAlerts(): Observable<MessageEvent> {
-    return this.alertsService.getAlertStream().pipe(
-      map((alert) => ({
-        data: alert,
-      })),
-    );
-  }
+
 }
